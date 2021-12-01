b2d7f2db09d54a195188d49162ba22d1a25c3f96
Ashish Sarode <adsarode@gmail.com>
2021-12-01 13:23:02 +0530

    Update FindAndRemoveFilesInDir.py

--- Utils/FindAndRemoveFilesInDir.py
+++ Utils/FindAndRemoveFilesInDir.py
@@ -1,11 +1,7 @@
 import glob
 import os
 
-# search files like abc.txt, abd.txt
- #'D:\ProjectData\BriusData\TestData\BracketTestScanned'
-#path = 'C:\SOURCE_BASE\Brius\Data\BracketTestScanned\**\*Dumps'
-#fileDump= glob.glob(path+'\**\*.stl*')
-pattern = 'C:\SOURCE_BASE\Brius\Data\BracketTestScanned\**\*Dumps*\**\*.stl*'
+pattern = 'C:\SOURCE_BASE\Data\**\*Dumps*\**\*.stl*'
 for item in glob.iglob(pattern, recursive=True):
     print (item)
     #os.remove(item)
